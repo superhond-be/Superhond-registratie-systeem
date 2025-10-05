9f91d68e9a973079216a13fa23248af59a140953
superhond-be <paul.thijs1@icloud.com>
2025-10-05 22:39:43 +0200

    Update config.js

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 <script>
-  // Zet hier je eigen Web-app URL
+  // 🌍 Centrale configuratie voor Superhond
   window.SUPERHOND_DATA = {
-    CLASSES_URL: 'https://script.google.com/macros/s/…/exec' // <-- jouw URL
+    API_BASE: "https://script.google.com/macros/s/AKfycbzprHaU1ukJT03YLQ6I5EzR1LOq_45tzWNLo-d92rJuwtRat6Qf_b8Ydt-0qoZBIctVNA/exec"
   };
 </script>
