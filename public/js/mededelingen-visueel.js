76721d9cda0f7f04038cb499d83b9c954f2bba63
superhond-be <paul.thijs1@icloud.com>
2025-10-16 19:42:55 +0200

    Create mededelingen-visueel.js

---
+++ public/js/mededelingen-visueel.js
@@ -0,0 +1,18 @@
+/**
+ * public/js/mededelingen-visueel.js
+ * Handige functies voor mededelingen-interface
+ */
+
+export function escapeHtml(s) {
+  return String(s).replace(/[&<>"']/g, c =>
+    ({ '&':'&amp;', '<':'&lt;', '>':'&gt;', '"':'&quot;', "'":'&#39;' }[c])
+  );
+}
+
+export function toArrayRows(x) {
+  if (Array.isArray(x)) return x;
+  if (x && Array.isArray(x.data)) return x.data;
+  if (x && Array.isArray(x.rows)) return x.rows;
+  if (x && Array.isArray(x.result)) return x.result;
+  return [];
+}
